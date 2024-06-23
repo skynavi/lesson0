2857809a461416e9d8d66933d5c15f8078a68b18
skynavi <56452788+skynavi@users.noreply.github.com>
2024-06-23 22:58:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+a = 'три' # Задача «Длина слова»
+print(len(a))
+first = 3 # Задача «Суммы и разности»
+second = 2
+summa = first + second
+diff = first - second
+print(summa)
+print(diff)
+print(first + second, first - second) # одной строкой два ответа
+third = 8 # first и second заданы выше! задача «Среднее арифметическое»
+mean = (first + second + third)/3
+print(mean)
+first_string = 'Вторник' # Задача «Простые строчки»
+second_string = 'Понедельник'
+print(second_string +', '+ first_string)
+a = 2 # Задача «Сложная формула»
+b = 3
+c = 4
+f = ((a * b) + (a * c))**3/2
+print(f)
